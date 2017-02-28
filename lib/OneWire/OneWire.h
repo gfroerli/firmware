bbdd22775dde78058e5580ed8a34d8bf280f1645
Raphael Nestler <raphael.nestler@gmail.com>
2017-02-28 23:03:49 +0100

    firmware: Add read/write bit in OneWire

--- lib/OneWire/OneWire.h
+++ lib/OneWire/OneWire.h
@@ -10,7 +10,10 @@ public:
     void reset();
 
     void write_byte(uint8_t byte);
+    void write_bit(bool bit);
     uint8_t read_byte();
+    bool read_bit();
+
 private:
     DigitalInOut _pin;
 };
