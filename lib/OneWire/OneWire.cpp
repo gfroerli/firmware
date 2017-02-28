a892acfab44820e5ec1a9760f2ea86ff61f9fe72
Raphael Nestler <raphael.nestler@gmail.com>
2017-02-28 23:03:49 +0100

    firmware: Add read/write bit in OneWire

--- lib/OneWire/OneWire.cpp
+++ lib/OneWire/OneWire.cpp
@@ -9,51 +9,54 @@ void OneWire::reset()
     wait_us(500);
 }
 
+void OneWire::write_bit(bool bit)
+{
+    if (bit) {
+        _pin.output();
+        _pin = 0;
+        wait_us(5);
+        _pin.input();
+        wait_us(60);
+    } else {
+        _pin.output();
+        _pin = 0;
+        wait_us(60);
+        _pin.input();
+    }
+}
+
 // output byte d (least sig bit first).
 void OneWire::write_byte(unsigned char d)
 {
-    unsigned char n;
-
-    for (n=8; n!=0; n--)
+    for (uint8_t n=8; n!=0; n--)
     {
-        // test least sig bit
-        if (d & 0x01)
-        {
-            _pin.output();
-            _pin = 0;
-            wait_us(5);
-            _pin.input();
-            wait_us(60);
-        }
-        else
-        {
-            _pin.output();
-            _pin = 0;
-            wait_us(60);
-            _pin.input();
-        }
-        // now the next bit is in the least sig bit position.
+        write_bit(d & 0x01);
         d = d >> 1;
     }
+}
 
+bool OneWire::read_bit() {
+    _pin.output();
+    _pin = 0;
+    wait_us(5);
+    _pin.input();
+    wait_us(5);
+    bool b = _pin;
+    wait_us(50);
+
+    return b;
 }
 
 // read byte, least sig byte first
-unsigned char OneWire::read_byte()
+uint8_t OneWire::read_byte()
 {
     uint8_t d = 0;
 
     for (uint8_t n = 0; n < 8; n++)
     {
-        _pin.output();
-        _pin = 0;
-        wait_us(5);
-        _pin.input();
-        wait_us(5);
-        uint8_t b = _pin;
-        wait_us(50);
-        // shift d to right and insert b in most sig bit position
-        d = (d >> 1) | (b<<7);
+        if (read_bit()) {
+            d |= 1 << n;
+        }
     }
     return d;
 }
