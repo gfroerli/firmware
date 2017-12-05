13aced975891c8333abe5f2f4a6eed0fc9e8f35b
Raphael Nestler <raphael.nestler@gmail.com>
2017-12-05 22:22:31 +0100

    Extract SupplyMonitor conversion method
    Useful when bypassing the SupplyMonitor for testing of the measurement
    with different timings.

--- src/SupplyMonitor.cpp
+++ src/SupplyMonitor.cpp
@@ -5,5 +5,5 @@ float SupplyMonitor::get_supply_voltage()
     enable();
     float input = read_input();
     disable();
-    return input * 3.3f / 9.31 * (9.31f + 6.04f);
+    return convert_input(input);
 }
