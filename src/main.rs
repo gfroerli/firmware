a2a9275649395f655211debbd76753b801d07bb0
Raphael Nestler <raphael.nestler@gmail.com>
2017-12-29 23:01:43 +0100

    Add clock_setup
    More or less inspired by the mbed code.

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,53 @@ fn sleep(cycles: u32) {
     }
 }
 
+const SYSOSCCTRL_VAL: u32 = 0x00000000; // Reset: 0x000
+const SYSPLLCTRL_VAL: u32 = 0x00000023; // Reset: 0x000
+const SYSPLLCLKSEL_VAL: u32 = 0x00000001; // Reset: 0x000
+const MAINCLKSEL_VAL: u32 = 0x00000003; // Reset: 0x000
+const SYSAHBCLKDIV_VAL: u32 = 0x00000001; // Reset: 0x001
+
+fn clock_setup() {
+    unsafe {
+        let syscon = &*lpc11uxx::SYSCON.get();
+
+        syscon.pdruncfg.modify(|_,w| w.sysosc_pd().powered());
+        syscon.sysoscctrl.write(|w| w.bits(SYSOSCCTRL_VAL));
+        sleep(200);
+
+        syscon.syspllclksel.write(|w| w.bits(SYSPLLCLKSEL_VAL));
+        syscon.syspllclkuen.write(|w| w.bits(0x01));
+        syscon.syspllclkuen.write(|w| w.bits(0x00));
+        syscon.syspllclkuen.write(|w| w.bits(0x01));
+        // wait until updated
+        while syscon.syspllclkuen.read().bits() & 0x01 == 0 {
+        }
+
+        syscon.syspllctrl.write(|w| w.bits(SYSPLLCTRL_VAL));
+        syscon.pdruncfg.modify(|_,w| w.syspll_pd().powered());
+
+
+        syscon.mainclksel.write(|w| w.bits(MAINCLKSEL_VAL));
+        syscon.mainclkuen.write(|w| w.bits(0x01));
+        syscon.mainclkuen.write(|w| w.bits(0x00));
+        syscon.mainclkuen.write(|w| w.bits(0x01));
+        // wait until updated
+        while syscon.mainclkuen.read().bits() & 0x01 == 0 {
+        }
+        syscon.sysahbclkdiv.write(|w| w.bits(SYSAHBCLKDIV_VAL));
+
+        syscon.pdruncfg.modify(|_,w| w
+            .usbpad_pd().usb_transceiver_poweered_down()
+            .usbpll_pd().powered_down()
+        );
+
+        syscon.sysahbclkctrl.modify(|_,w| w.iocon().enable());
+    }
+}
 
 fn main() {
     hprintln!("Hello, world!");
+    clock_setup();
 
     let syscon = lpc11uxx::SYSCON.get();
 
