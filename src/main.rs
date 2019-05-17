1d7647ad84def9be864091145d8fefef76ee380f
Danilo Bargen <mail@dbrgn.ch>
2019-05-17 14:23:41 +0200

    Nucleo board blinky

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use cortex_m_rt::entry;
 use cortex_m_semihosting as sh;
 use sh::hio;
 use stm32l0xx_hal as hal;
-use stm32l0xx_hal::rcc::RccExt;
+use stm32l0xx_hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
@@ -24,14 +24,20 @@ fn main() -> ! {
     writeln!(stdout, "Initializing Delay").unwrap();
     let syst = p.SYST;
     let rcc_config = hal::rcc::Config::default();
-    let rcc = dp.RCC.freeze(rcc_config);
+    let mut rcc = dp.RCC.freeze(rcc_config);
     let mut delay = hal::delay::Delay::new(syst, rcc.clocks);
+
+    writeln!(stdout, "Initializing GPIO").unwrap();
+    let gpiob = dp.GPIOB.split(&mut rcc);
+    let mut led = gpiob.pb3.into_push_pull_output();
 
     writeln!(stdout, "Starting loop").unwrap();
     loop {
         writeln!(stdout, "Hello, ").unwrap();
+        led.set_high();
         delay.delay(hal::time::MicroSeconds(1_000_000));
         writeln!(stdout, "world!").unwrap();
+        led.set_low();
         delay.delay(hal::time::MicroSeconds(1_000_000));
     }
 }
