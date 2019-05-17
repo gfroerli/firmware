496de3218666f1d5e4264b941231d0c1b8796c96
Danilo Bargen <mail@dbrgn.ch>
2019-05-17 20:54:32 +0200

    Nucleo board serial communication

--- src/main.rs
+++ src/main.rs
@@ -31,13 +31,29 @@ fn main() -> ! {
     let gpiob = dp.GPIOB.split(&mut rcc);
     let mut led = gpiob.pb3.into_push_pull_output();
 
+    writeln!(stdout, "Initializing USART").unwrap();
+    let gpioa = dp.GPIOA.split(&mut rcc);
+    let mut serial = hal::serial::Serial::usart2(
+        dp.USART2,
+        (gpioa.pa9.into_floating_input(), gpioa.pa10.into_floating_input()),
+        hal::serial::Config {
+            baudrate: hal::time::Bps(9600),
+            wordlength: hal::serial::WordLength::DataBits8,
+            parity: hal::serial::Parity::ParityNone,
+            stopbits: hal::serial::StopBits::STOP1,
+        },
+        &mut rcc,
+    ).unwrap();
+
     writeln!(stdout, "Starting loop").unwrap();
     loop {
         writeln!(stdout, "Hello, ").unwrap();
         led.set_high();
+        serial.write_char('a').unwrap();
         delay.delay(hal::time::MicroSeconds(1_000_000));
         writeln!(stdout, "world!").unwrap();
         led.set_low();
+        serial.write_char('b').unwrap();
         delay.delay(hal::time::MicroSeconds(1_000_000));
     }
 }
