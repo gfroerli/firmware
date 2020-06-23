b345bd936a327eedbd811ee77b782873d1491248
Raphael Nestler <raphael.nestler@gmail.com>
2020-06-23 22:27:46 +0200

    Add ds18b20 crate and read temperature of attached sensor

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,17 @@ use stm32l0xx_hal::gpio::{
 use stm32l0xx_hal::prelude::*;
 use stm32l0xx_hal::{self as hal, i2c::I2c, pac, serial, time};
 
-use embedded_hal::blocking::delay::DelayUs;
+use embedded_hal::blocking::delay::{DelayUs, DelayMs};
 use embedded_hal::digital::v2::{InputPin, OutputPin};
-use one_wire_bus::OneWire;
+use one_wire_bus::{OneWire, OneWireResult};
+
+use ds18b20::{self, Ds18b20, Resolution};
 
 fn find_devices<P, E>(
     delay: &mut impl DelayUs<u16>,
     tx: &mut impl Write,
     one_wire_pin: P,
-)
+) -> OneWire<P>
     where
         P: OutputPin<Error=E> + InputPin<Error=E>,
         E: Debug
@@ -39,8 +41,47 @@ fn find_devices<P, E>(
         writeln!(tx, "Found device at address {:?} with family code: {:#x?}",
                  device_address, device_address.family_code()).unwrap();
     }
+    one_wire_bus
 }
 
+fn get_temperature<P, E>(
+    delay: &mut (impl DelayUs<u16> + DelayMs<u16>),
+    tx: &mut impl Write,
+    one_wire_bus: &mut OneWire<P>,
+) -> OneWireResult<(), E>
+    where
+        P: OutputPin<Error=E> + InputPin<Error=E>,
+        E: Debug
+{
+    // initiate a temperature measurement for all connected devices
+    ds18b20::start_simultaneous_temp_measurement(one_wire_bus, delay)?;
+
+    // wait until the measurement is done. This depends on the resolution you specified
+    // If you don't know the resolution, you can obtain it from reading the sensor data,
+    // or just wait the longest time, which is the 12-bit resolution (750ms)
+    Resolution::Bits12.delay_for_measurement_time(delay);
+
+    // iterate over all the devices, and report their temperature
+    let mut search_state = None;
+    loop {
+        if let Some((device_address, state)) = one_wire_bus.device_search(search_state.as_ref(), false, delay)? {
+            search_state = Some(state);
+            if device_address.family_code() != ds18b20::FAMILY_CODE {
+                // skip other devices
+                continue;
+            }
+            // You will generally create the sensor once, and save it for later
+            let sensor = Ds18b20::new(device_address)?;
+
+            // contains the read temperature, as well as config info such as the resolution used
+            let sensor_data = sensor.read_data(one_wire_bus, delay)?;
+            writeln!(tx, "Device at {:?} is {}°C", device_address, sensor_data.temperature).ok();
+        } else {
+            break;
+        }
+    }
+    Ok(())
+}
 
 
 mod leds;
@@ -129,7 +170,8 @@ const APP: () = {
         .unwrap();
 
         let one_wire_pin = gpioa.pa6.into_open_drain_output();
-        find_devices(&mut delay, &mut debug, one_wire_pin);
+        let mut one_wire_bus = find_devices(&mut delay, &mut debug, one_wire_pin);
+        get_temperature(&mut delay, &mut debug, &mut one_wire_bus).ok();
 
 
         // Initialize LEDs
