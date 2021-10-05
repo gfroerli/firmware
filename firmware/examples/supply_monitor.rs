675dd7ca110f74a17b151069a9607f84058cc6d8
Raphael Nestler <raphael.nestler@gmail.com>
2021-10-05 21:34:48 +0200

    WIP: supply monitor example

--- firmware/examples/supply_monitor.rs
+++ firmware/examples/supply_monitor.rs
@@ -11,13 +11,11 @@ use stm32l0xx_hal as hal;
 use stm32l0xx_hal::prelude::*;
 
 use gfroerli_firmware::supply_monitor;
-use gfroerli_common::measurement::U12;
 
 #[entry]
 fn main() -> ! {
     let p = cortex_m::Peripherals::take().unwrap();
     let dp = stm32l0xx_hal::pac::Peripherals::take().unwrap();
-
 
     let syst = p.SYST;
     let mut rcc = dp.RCC.freeze(hal::rcc::Config::hsi16());
@@ -31,10 +29,9 @@ fn main() -> ! {
         dp.USART2,
         gpioa.pa2.into_floating_input(),
         gpioa.pa3.into_floating_input(),
-        
         //gpioa.pa9.into_floating_input(),
         //gpioa.pa10.into_floating_input(),
-        
+
         //gpiob.pb6.into_floating_input(),
         //gpiob.pb7.into_floating_input(),
         hal::serial::Config {
@@ -49,21 +46,34 @@ fn main() -> ! {
 
     // Initialize supply monitor
     let adc = dp.ADC.constrain(&mut rcc);
+
     let a1 = gpioa.pa1.into_analog();
     let adc_enable_pin = gpioa.pa5.into_push_pull_output().downgrade();
     let mut supply_monitor = supply_monitor::SupplyMonitor::new(a1, adc, adc_enable_pin);
 
-
     loop {
         let v_supply = supply_monitor.read_supply_raw();
         if let Some(v) = v_supply {
-            writeln!(serial, "{:?}", v);
+            writeln!(serial, "{}", v);
 
             // real ca. 0.7V@3.3V
-            let v_input =  (v as f32) / 4095.0 * 3.3;
-            let v_supply_converted =  (v as f32) / 4095.0 * 3.3 / 2.7 * (10.0 + 2.7);
+            let v_input = (v as f32) / 4095.0 * 3.3;
+            let v_supply_converted = (v as f32) / 4095.0 * 3.3 / 2.7 * (10.0 + 2.7);
 
-            writeln!(serial, "{} -> {:?}", v_input, v_supply_converted);
+            writeln!(serial, "{} -> {}", v_input, v_supply_converted).unwrap();
+        }
+
+        /*
+        if let Some(v) = supply_monitor.read_vref_raw() {
+            let v_input = (v as f32) / 4095.0 * 3.3;
+            let v_dda = supply_monitor::SupplyMonitor::convert_vrefint_to_vdda(v);
+
+            writeln!(serial, "VDDA: {} ({}) -> {}", v, v_input, v_dda).unwrap();
+        }
+        */
+
+        if let Some(vdd) = supply_monitor.read_supply() {
+            writeln!(serial, "VDD: {:?}", vdd);
         }
 
         //serial.write_char('a').unwrap();
